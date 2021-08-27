12aa983a290a4e2d45646c679f8bf382a9fe38fb
Gusenkoof <Leha148@gmail.com>
2021-08-27 22:01:47 +0200

    27.08

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.Argument;
 import io.restassured.specification.RequestSpecification;
@@ -9,12 +10,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestInstance;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("API тесты")
@@ -24,36 +28,57 @@ public class TestPet {
     private static RequestSpecification reSpec = SpecificationApi.getRequestSpecification();
 
 
+
     @Test
     @Order(1)
     public void testPost(){
         TagPet tagPet = new TagPet(44444, "British cat");
         Category category = new Category(8, "Cat");
         Pet pet = new Pet(8965, category, "Missi", new ArrayList<>(), new ArrayList<>(Collections.singletonList(tagPet)), "available");
-        Response response = given()
+        given()
                 .spec(reSpec)
+                .contentType(ContentType.JSON)
                 .body(pet)
-                .post(EndPoints.PET);
+                .post(EndPoints.PET)
+                .then()
+                .assertThat()
+                .body("id", equalTo(8965))
+                .assertThat()
+                .body("name", equalTo("Missi"));
     }
 
     @Test
     @Order(2)
     public void testGet(){
-        Response response = given()
+         given()
                 .spec(reSpec)
+                .contentType(ContentType.JSON)
                 .when()
-                .get("/" + EndPoints.PET+"/5489");
+                .get("/" + EndPoints.PET+"/42805")
+                .then()
+                .body("name", equalTo("Cleo"));
 
-        response.prettyPrint();
+
     }
 
     @Test
     @Order(3)
+    public void testGet_2() {
+        given()
+                .spec(reSpec)
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    @Order(4)
     public void testDelete(){
         Response response = given()
                 .spec(reSpec)
                 .when()
-                .delete("/" + EndPoints.PET+"/2295");
+                .delete("/" + EndPoints.PET+"/2295"); // или 42805
 
         response.prettyPrint();
     }
